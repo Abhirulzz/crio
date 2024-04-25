e58ba8aa318a627015703deee7d6f378b9b40e9f
a864855 <a864855@DESKTOP-OH9I28E>
2024-04-25 19:00:12 +0530

    nqueen

--- BuilderPattern/src/com/example/crio/dsa4/NQueen.java
+++ BuilderPattern/src/com/example/crio/dsa4/NQueen.java
@@ -6,14 +6,17 @@ import java.util.List;
 
 public class NQueen {
 
-	    public static List < List < String >> solveNQueens(int n) {
+	    public static String solveNQueens(int n) {
 	        char[][] board = new char[n][n];
 	        for (int i = 0; i < n; i++)
 	            for (int j = 0; j < n; j++)
-	                board[i][j] = '.';
+	                board[i][j] = '0';
 	        List < List < String >> res = new ArrayList < List < String >> ();
-	        dfs(0, board, res);
-	        return res;
+	        StringBuilder result = new StringBuilder();
+	        if (dfs(0, board, res,result))
+	        return result.toString();
+	        else
+	        	return "No Solution Exists";
 	    }
 
 	    static boolean validate(char[][] board, int row, int col) {
@@ -28,37 +31,48 @@ public class NQueen {
 	        row = duprow;
 	        col = dupcol;
 	        while (col >= 0) {
-	            if (board[row][col] == 'Q') return false;
+	            if (board[row][col] == '1') return false;
 	            col--;
 	        }
 
 	        row = duprow;
 	        col = dupcol;
 	        while (col >= 0 && row < board.length) {
-	            if (board[row][col] == 'Q') return false;
+	            if (board[row][col] == '1') return false;
 	            col--;
 	            row++;
 	        }
 	        return true;
 	    }
 
-	    static void dfs(int col, char[][] board, List < List < String >> res) {
+	    static boolean dfs(int col, char[][] board, List < List < String >> res,StringBuilder result) {
 	        if (col == board.length) {
 	            res.add(construct(board));
-	            return;
+	            generateBoard(board,result);
+	            return true;
 	        }
 
 	        for (int row = 0; row < board.length; row++) {
 	            if (validate(board, row, col)) {
 	                board[row][col] = '1';
-	                dfs(col + 1, board, res);
+	                dfs(col + 1, board, res,result);
 	                board[row][col] = '0';
 	            }
 	        }
+	        return false;
 	    }
 
 
-
+	    private static void generateBoard(char[][] board, StringBuilder result) {
+	        for (int i = 0; i < board.length; i++) {
+	           
+	                    result.append(new String(board[i]));
+	                    result.append("\n");
+	            }
+	            //result.append("\n");
+	        }
+	    
+	    
 	    static List < String > construct(char[][] board) {
 	        List < String > res = new LinkedList < String > ();
 	        for (int i = 0; i < board.length; i++) {
@@ -68,8 +82,9 @@ public class NQueen {
 	        return res;
 	    }
 	    public static void main(String args[]) {
-	        int N = 4;
-	        List < List < String >> queen = solveNQueens(N);
+	        int N = 2;
+	        String NQueen = solveNQueens(N);
+	        /*List < List < String >> queen = solveNQueens(N);
 	        int i = 1;
 	        for (List < String > it: queen) {
 	            System.out.println("Arrangement " + i);
@@ -78,8 +93,8 @@ public class NQueen {
 	            }
 	            System.out.println();
 	            i += 1;
-	        }
-	        
+	        }*/
+	     System.out.println("NQueen" + NQueen);   
 	    }
 	}
 
